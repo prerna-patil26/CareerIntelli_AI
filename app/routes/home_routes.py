9a6ca64b52f138934d76eb34690c53e639158327
Shivani Pagar <shivanipagar19@gmail.com>
2026-04-16 00:25:10 +0530

    index page name

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -3,10 +3,9 @@ from app.database.models import User
 
 home_bp = Blueprint("home", __name__)
 
-
 @home_bp.route("/")
-def home():
-    return render_template("login.html")
+def index():
+    return render_template("index.html")
 
 
 @home_bp.route("/login")
